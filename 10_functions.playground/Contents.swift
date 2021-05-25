5ccabe67ff25dff7bde63313d9f00f4790826ab6
Francisco Leví <francisco.levi.md@gmail.com>
2021-05-25 14:04:58 -0500

    Valores de Retorno, Optionals y Parámetros de Salida

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -24,3 +24,47 @@ func greeting(person:String, isMale:Bool) -> String {
 }
 greeting(person: "Emilia", isMale: false)
 greeting(person: "Leví", isMale: true)
+
+// Sin retorno
+func greet2(person:String) {
+    print("Hola \(person)")
+}
+greet2(person: "Jessica Irais")
+greet2(person: "Fatima")
+
+// Retornos
+func printAndCount(string:String) -> Int{
+    print(string)
+    return string.count
+}
+printAndCount(string: "Saluditos")
+
+func printWithoutCounting(string:String){
+    let _ = printAndCount(string: string)
+}
+
+printAndCount(string: "Buenos días")
+printWithoutCounting(string: "Buenos días")
+
+// Retorno de tuplas
+func minMax(array:[Int]) -> (min:Int, max:Int)?{
+    // Retorno de optional
+    if array.isEmpty{
+        return nil
+    }
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+let bounds = minMax(array: [34, 3 , 46, 12, -3, 3, 5, 0, 5, 15, -43])
+print("Los valores se hallan entre \(bounds!.min) y \(bounds!.max)") // Unwrapping no es la mejor técnica.
+
+// Retorno de optional
+minMax(array: [])
