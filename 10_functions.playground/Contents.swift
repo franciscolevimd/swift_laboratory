77d88603ba08d6789e64c7a0d605f967932f386e
Francisco Leví <francisco.levi.md@gmail.com>
2021-05-25 13:07:22 -0500

    Declaración de funciones.

---
+++ 10_functions.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// Declarara un afunción.
+func greeting(person:String) -> String {
+    let greeting = "¡Hola, \(person)!"
+    return greeting
+}
+
+greeting(person: "Fatsy")
+greeting(person: "Emilia")
+greeting(person: "Sakurita")
+
