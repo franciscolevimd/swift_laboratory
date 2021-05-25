347f9b35a0b932c932f85b95ddeee99013b65df8
Francisco Leví <francisco.levi.md@gmail.com>
2021-05-25 13:44:52 -0500

    Parámetros de entrada

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -5,8 +5,22 @@ func greeting(person:String) -> String {
     let greeting = "¡Hola, \(person)!"
     return greeting
 }
-
 greeting(person: "Fatsy")
 greeting(person: "Emilia")
 greeting(person: "Sakurita")
 
+// Parámetros de entrada.
+func sayHelloWorld() -> String {
+    return "Hello world"
+}
+sayHelloWorld()
+
+func greeting(person:String, isMale:Bool) -> String {
+    if isMale{
+        return "Bienvenido caballero \(person)"
+    } else {
+        return "Bienvenida señorita \(person)"
+    }
+}
+greeting(person: "Emilia", isMale: false)
+greeting(person: "Leví", isMale: true)
